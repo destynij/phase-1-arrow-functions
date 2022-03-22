c53b660dd5084472dd24077a9d2eb4bbd6e5fe59
Destyni <Destyninjohnson20@icloud.com>
2022-03-22 18:04:46 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide (number1 = 2000, number2 = 100) {
+console.log(number1 / number2);
+return 20
+}
+
+const square = number => {
+    console.log(number * number);
+    return 4
+}
+
+const add = (number5, number6) => {
+    console.log(number5 + number6);
+    return 7
+}
